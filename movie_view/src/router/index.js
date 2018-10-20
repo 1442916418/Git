dc446c75f3b7f3ddd6e48d8d7cc1b0dcc8f1b77d
Yu shuisheng <1442916418@qq.com>
2018-10-20 18:18:44 +0800

    增加HelloWorld页面 2018/10/20

--- movie_view/src/router/index.js
+++ movie_view/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import NewHello from '@/components/NewHello'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/HelloWord',
+      name: 'HelloWorld',
+      component: NewHello
     }
   ]
 })
