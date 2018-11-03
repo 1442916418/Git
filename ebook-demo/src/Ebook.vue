46c89cfb0cbaed6f2716fcf3d2e32e530651e6d0
Yu shuisheng <1442916418@qq.com>
2018-11-03 21:50:16 +0800

    电子书完成

--- ebook-demo/src/Ebook.vue
+++ ebook-demo/src/Ebook.vue
@@ -13,6 +13,17 @@
         </div>
         <menu-bar
         :ifTitleAndMenuShow="ifTitleAndMenuShow"
+        :fontSizeList="fontSizeList"
+        :defaultFontSize="defaultFontSize"
+        @setFontSize="setFontSize"
+        :themeList="themeList"
+        :defaultTheme="defaultTheme"
+        @setTheme="setTheme"
+        :bookAvailable="bookAvailable"
+        @onProgressChange="onProgressChange"
+        :navigation="navigation"
+        @jumpTo="jumpTo"
+        :parentProgress="progress"
         ref="MenuBar"></menu-bar>
     </div>
 </template>
@@ -30,28 +41,136 @@ export default {
     },
     data () {
         return {
-            ifTitleAndMenuShow: false
+            ifTitleAndMenuShow: false,
+            fontSizeList: [
+                { fontSize: 12 },
+                { fontSize: 14 },
+                { fontSize: 16 },
+                { fontSize: 18 },
+                { fontSize: 20 },
+                { fontSize: 22 },
+                { fontSize: 24 }
+            ],
+            defaultFontSize: 16,
+            themeList: [
+                {
+                    name: 'default',
+                    style: {
+                        body: {
+                            'color': '#000', 'background': '#fff'
+                        }
+                    }
+                },
+                {
+                    name: 'eye',
+                    style: {
+                        body: {
+                            'color': '#000', 'background': '#ceeaba'
+                        }
+                    }
+                },
+                {
+                    name: 'nigth',
+                    style: {
+                        body: {
+                            'color': '#fff', 'background': '#000'
+                        }
+                    }
+                },
+                {
+                    name: 'gold',
+                    style: {
+                        body: {
+                            'color': '#000 ', 'background': 'rgb(241, 236, 226)'
+                        }
+                    }
+                }
+            ],
+            defaultTheme: 0,
+            // 图书是否处于可用状态
+            bookAvailable: false,
+            navigation: {},
+            progress: 0
         }
     },
     methods: {
+        prevPage() {
+            if (this.rendition) {
+                this.rendition.prev().then(() => {
+                    // 获取当前的位置信息
+                    const currentLocation = this.rendition.currentLocation()
+                    // 获取当前位置的进度百分比
+                    this.progress = this.location.percentageFromCfi(currentLocation.start.cfi)
+                    // 转化成0-100的数字
+                    this.progress = Math.round(this.progress * 100)
+                })
+            }
+        },
+        nextPage() {
+            if (this.rendition) {
+                // this.rendition.next()
+                this.rendition.next().then(() => {
+                    // 获取当前位置信息
+                    const currentLocation = this.rendition.currentLocation()
+                    // 获取当前位置的进度百分比
+                    this.progress = this.locations.percentageFromCfi(currentLocation.start.cfi)
+                    // 转化成0-100的数字
+                    this.progress = Math.round(this.progress * 100)
+                })
+            }
+        },
+        // 根据链接跳转到指定位置
+        jumpTo(href) {
+            this.rendition.display(href)
+            this.hideTitleAndMenu()
+        },
+        hideTitleAndMenu() {
+            // 隐藏标题栏和菜单栏
+            this.ifTitleAndMenuShow = false
+            // 隐藏菜单栏弹出的设置栏
+            this.$refs.menuBar.hideSetting()
+            // 隐藏目录
+            this.$refs.menuBar.hideContent()
+        },
+        // progress 进度条的数值 (0-100)
+        onProgressChange(progress) {
+            const percentage = progress / 100
+            const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
+            this.rendition.display(location)
+        },
+        setTheme(index) {
+            this.themes.select(this.themeList[index].name)
+            this.defaultTheme = index
+        },
+        registerTheme() {
+            this.themeList.forEach(theme => {
+                this.themes.register(theme.name, theme.style)
+            })
+        },
+        setFontSize(fontSize) {
+            this.defaultFontSize = fontSize
+            if (this.themes) {
+                this.themes.fontSize(fontSize + 'px')
+            }
+        },
         toggleTitleAndMenu() {
             this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
             if (!this.ifTitleAndMenuShow) {
                 this.$refs.MenuBar.hideSetting()
             }
         },
-        prevPage() {
-            // 调用 Rendition.prev
-            if (this.rendition) {
-                this.rendition.prev()
-            }
-        },
-        nextPage() {
-            // 调用 Rendition.next
-            if (this.rendition) {
-                this.rendition.next()
-            }
-        },
+        // prevPage() {
+        //     // 调用 Rendition.prev
+        //     if (this.rendition) {
+        //         this.rendition.prev()
+        //     }
+        // },
+        // nextPage() {
+        //     // 调用 Rendition.next
+        //     if (this.rendition) {
+        //         this.rendition.next()
+        //     }
+        // },
         // 电子书的解析和渲染
         showEpub () {
             // 生成Book对象
@@ -63,6 +182,23 @@ export default {
             })
             // 通过Rendtion.display渲染电子书
             this.rendition.display()
+            // 获取Theme对象
+            this.themes = this.rendition.themes
+            // 设置默认字体
+            this.setFontSize(this.defaultFontSize)
+            // this.themes.register(name, styles)
+            // this.themes.select(name)
+            this.registerTheme()
+            this.setTheme(this.defaultTheme)
+            // 获取 Locations 对象
+            // 通过 epubjs 的钩子函数来实现
+            this.book.ready.then(() => {
+                this.navigation = this.book.navigation
+                return this.book.locations.generate()
+            }).then(result => {
+                this.locations = this.book.locations
+                this.bookAvailable = true
+            })
         }
     },
     mounted () {
