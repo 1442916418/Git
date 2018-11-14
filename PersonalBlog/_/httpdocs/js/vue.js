dbb258f6b874793b6b7cdb8df97c794bef38b3bd
Yu shuisheng <1442916418@qq.com>
2018-11-14 22:35:40 +0800

    个人网站 首页前两页

---
+++ PersonalBlog/_/httpdocs/js/vue.js
@@ -0,0 +1,25 @@
+var vue = new Vue({
+    el: "#app",
+    data: {
+      detailsOne: [
+        {
+            phrase:'穷在闹市无人问，富在深山有远亲',
+            name:'我叫余水生',
+            trainee:'一名前端工程师实习生',
+            email:'1442916418@qq.com'
+        }
+      ],
+      detailsTwo: [
+        {
+            introduce:'自我介绍',
+            selfIntroduction:'本人初学前端，有不足之处，请多多包涵!',
+            birthday:'1997.6.21',
+            education:'安徽新华电脑学院',
+            address:'安徽·合肥',
+            emailtwo:'1442916418@qq.com',
+            phone:'+86 17354181586',
+            company:'安徽·合肥'
+        }
+      ]
+    }
+  })
