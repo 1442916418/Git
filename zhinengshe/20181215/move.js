c9f64dd5356bd8ade5a00ce8615e6f18aa6beeb2
Yu shuisheng <1442916418@qq.com>
2018-12-15 21:24:19 +0800

    JS 函数运动框架

---
+++ zhinengshe/20181215/move.js
@@ -0,0 +1,68 @@
+// 运动框架
+//
+function startMove(obj, attr, iTarget, funEnd)
+{
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function(){
+        var cur = 0;
+
+        if (attr == 'opacity')
+        {
+            cur =Math.round(parseFloat(getStyle(obj, attr))*100);
+        }
+        else
+        {
+            cur = parseInt(getStyle(obj, attr));
+        }
+
+        var speed = (iTarget-cur) / 6;
+        speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
+
+        if (cur == iTarget)
+        {
+            clearInterval(obj.timer);
+
+            if (funEnd) funEnd();
+        }
+        else
+        {
+            if (attr == 'opacity')
+            {
+                obj.style.opacity = (cur+speed) / 100;
+            }
+            else
+            {
+                obj.style[attr] = cur + speed + 'px';
+            }
+        }
+    }, 30);
+}
+
+// 获取样式
+function getStyle(obj, name)
+{
+    if (obj.currentStyle)
+    {
+        return obj.currentStyle[name];
+    }
+    else
+    {
+        return getComputedStyle(obj, false)[name];
+    }
+}
+
+function getByClass(oParent, sClass)
+{
+    var aEle = document.getElementsByTagName('*');
+    var aResult = [];
+
+    for (var i = 0; i < aEle.length; i++)
+    {
+        if (aEle[i].className == sClass)
+        {
+            aResult.push(aEle[i]);
+        }
+    }
+    
+    return aResult;
+}
