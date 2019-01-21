232c36599e65e968c0d8ba7adaf1c8b5c640d070
Yu shuisheng <1442916418@qq.com>
2019-01-21 20:53:19 +0800

    博客页面修改

---
+++ BlogPageSettings/2/Chinese.js
@@ -0,0 +1,28 @@
+/* 鼠标特效 */
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("❤貂蝉❤","❤露娜❤","❤曹操❤","❤张飞❤","❤百里守约❤","❤妲己❤","❤孙悟空❤","❤后羿❤","❤虞姬❤","❤项羽❤","❤鬼谷子❤","❤赵云❤");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+    });
+});
